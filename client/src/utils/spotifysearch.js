a22da4a88ac74eabb3b43e03b710128510329360
Brian Adams <bpeteradams@gmail.com>
2021-06-29 22:01:35 -0400

    added basic spotify search functionality to the front end

---
+++ client/src/utils/spotifysearch.js
@@ -0,0 +1,61 @@
+import dotenv from 'dotenv';
+import axios from 'axios';
+import qs from 'qs';
+
+
+const getSpotifyToken = async () => {
+  const CLIENT_ID = process.env.REACT_APP_SPOTIFY_CLIENT_ID
+const CLIENT_SECRET = process.env.REACT_APP_SPOTIFY_CLIENT_SECRET
+  var client_id = CLIENT_ID; // Your client id
+  var client_secret = CLIENT_SECRET; // Your secret
+  const headers = {
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    auth: {
+      username: client_id,
+      password: client_secret,
+    },
+  };
+  const data = {
+    grant_type: 'client_credentials',
+  };
+
+  try {
+    const response = await axios.post(
+      'https://accounts.spotify.com/api/token',
+      qs.stringify(data),
+      headers
+    );
+    console.log(response.data.access_token);
+    return response.data.access_token;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const querySpotifyAPI = async (token) => {
+  const headers = {
+  headers:{
+    Accept: 'application/json',
+    'Content-Type': 'application/x-www-form-urlencoded',
+    Authorization: `Bearer ${token}`
+  },
+}
+  try {
+    const response = await axios.get('https://api.spotify.com/v1/search?query=haken&offset=0&type=album', headers)
+    console.log(response.data)
+    return response.data
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+const API = async () => {
+  const token = await getSpotifyToken();
+  const data = await querySpotifyAPI(token)
+  console.log(data);
+}
+
+export default API;
