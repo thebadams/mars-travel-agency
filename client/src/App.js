56a22694f4a45b9263cd399f8c6d83bb34c9ce32
Jasper Abarquez <78329142+KuyaJasper@users.noreply.github.com>
2021-07-01 19:19:33 -0400

    set up login component

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,10 @@
 import API from './utils/spotifysearch';
+import Login from './components/Login';
+
 function App() {
   return (
     <div className="App">
+      <Login />
       <header className="App-header">
         <button onClick={API}>Click ME</button>
       </header>
---
+++ client/src/components/Login.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Login = () => {
+    return (
+        <div>
+            <div>
+            <h2>Sign Up section</h2>
+            </div>
+            <div>
+            <h2>Login section</h2>
+            </div>
+        </div>
+    );
+};
+
+export default Login;
