d39c955c7ed1907558c8a4a68ff0c1421f5db619
Jasper Abarquez <78329142+KuyaJasper@users.noreply.github.com>
2021-07-06 13:19:21 -0400

    set up for pull

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -5,9 +5,21 @@ const Login = () => {
         <div>
             <div>
             <h2>Sign Up section</h2>
+            <form>
+                <label htmlFor="userName">User Name</label><br />
+                <input type="text" /> <br />
+                <label htmlFor="userName">Password</label><br />
+                <input type="text" /> <br />
+            </form>
             </div>
             <div>
             <h2>Login section</h2>
+            <form>
+                <label htmlFor="userName">User Name</label><br />
+                <input type="text" /> <br />
+                <label htmlFor="userName">Password</label><br />
+                <input type="text" /> <br />
+            </form>
             </div>
         </div>
     );
