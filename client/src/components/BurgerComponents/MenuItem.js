a37357a60579c6e40b4026ced5aec0242c4a9b2d
Clifford Morin <cemorin21@gmail.com>
2021-07-06 16:11:45 -0400

    pushing up burger with working links

--- client/src/components/BurgerComponents/MenuItem.js
+++ client/src/components/BurgerComponents/MenuItem.js
@@ -2,21 +2,60 @@ import * as React from "react";
 import { motion } from "framer-motion";
 import { ItemAnim } from "./BurgerAnimation";
 import { Link } from "react-router-dom";
+import styled from "styled-components";
 // import { useLocation } from "react-router-dom";
+//Icons
 
-const colors = ["#FF008C", "#D309E1", "#9C1AFF", "#7700FF", "#4400FF"];
 
+const colors = ["#FF008C", "#D309E1", "#9C1AFF", "#7700FF", "#4400FF", "#0d00ff"];
+const links = ["/", "/booking", "/weather", "/news", "/login", "/signup"]
+const text = ["Home", "Booking", "Weather", "News", "Login", "SignUp"]
 
 export const MenuItem = ({ i }) => {
-  const style = { border: `2px solid ${colors[i]}` };
+  const style = { border: `2px solid ${colors[i]}`, color: `${colors[i]}` };
+
   return (
+    <StyledItems>
     <motion.li
       variants={ItemAnim}
       whileHover={{ scale: 1.1 }}
       whileTap={{ scale: 0.95 }}
     >
-      <div className="icon" style={style} />
-      <div className="text" style={style} />
+      <Link to={`${links[i]}`}>
+      <div className="icon" style={style}/>
+      </Link>
+      <Link to={`${links[i]}`} className="text" style={style}>{`${text[i]}`}</Link>
     </motion.li>
+    </StyledItems>
   );
 };
+
+const StyledItems = styled.div`
+
+li {
+  list-style: none;
+  margin-bottom: 20px;
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+}
+
+.icon {
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  flex: 40px 0;
+  margin-right: 20px;
+}
+
+.text {
+  border-radius: 5px;
+  width: 200px;
+  height: 20px;
+  flex: 1;
+  font-size: 15px;
+  text-align: center;
+  text-decoration: none;
+}
+
+`;
