c00f89c61b1c588b7004627dd36fac7a4f939e52
Jasper Abarquez <78329142+KuyaJasper@users.noreply.github.com>
2021-07-06 21:04:01 -0400

    started weather page layout

--- client/src/pages/Weather.js
+++ client/src/pages/Weather.js
@@ -1,11 +1,31 @@
 import React from "react";
+import { Button } from '@material-ui/core';
+import styled from "styled-components";
+import WeatherCard from "../components/WeatherComponents/weatherCard"
 
-//Page Components
 
-//Animations
+const WeatherStyle = styled.div`
+
+width: 100vw;
+height: 100vh;
+display: flex;
+    justify-content: center;
+
+.weatherBtn{
+    background: red;
+}
+.weatherCard{
+    background: red;
+}
+`;
 
 const Weather = () => {
-    return <h1>Weather</h1>;
+    return(
+        <WeatherStyle>
+            <Button color="primary" className= "weatherBtn">Hello World</Button>
+            <WeatherCard className= "weatherCard" />
+        </WeatherStyle>
+    )
 };
 
 export default Weather;
