ee6a27603194c7033783df5033eb87dce994695b
Clifford Morin <cemorin21@gmail.com>
2021-07-07 03:17:13 -0400

    added facebook sign in, sign up button

--- client/src/components/loginComponents/LoginBox.js
+++ client/src/components/loginComponents/LoginBox.js
@@ -7,24 +7,26 @@ import Button from "@material-ui/core/Button";
 import TextField from "@material-ui/core/TextField";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
 import Checkbox from "@material-ui/core/Checkbox";
-
 import Grid from "@material-ui/core/Grid";
 import Box from "@material-ui/core/Box";
 import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
+//Icons
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFacebookSquare } from "@fortawesome/free-brands-svg-icons"
 
 function Copyright() {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
       {"Copyright © "}
-      <Link color="inherit" href="https://material-ui.com/">
-        Your Website
-      </Link>{" "}
+      <a href="https://github.com/thebadams/mars-travel-agency" target="_blank" rel="noreferrer">
+        Mars Travel Agency
+      </a>{" "}
       {new Date().getFullYear()}
       {"."}
-    </Typography>
+    </Typography> 
   );
 }
 
@@ -75,10 +77,26 @@ const LoginBox = () => {
           >
             Sign In
           </Button>
-          <Grid container>
-            <Grid item>
-              <Link to="/signup">Don't have an account, Sign Up here!</Link>
+          <Grid 
+            container  
+            direction="row"
+            justifyContent="space-around"
+            alignItems="center"
+          >
+          <Grid item xs={6}>
+              <Button
+                variant="contained"
+                color="default"
+                className={classes.button}
+              >
+                <FontAwesomeIcon icon={faFacebookSquare} color="blue" size="3x"/>
+              Sign In
+              </Button>
             </Grid>
+            <Grid item xs={6}>
+              <Link to="/signup" style={{textDecoration: "none"}}>Don't have an account, Sign Up here!</Link>
+            </Grid>
+            
           </Grid>
         </form>
       </div>
@@ -116,6 +134,11 @@ const useStyles = makeStyles((theme) => ({
     background: "linear-gradient(145deg, #04d1db, #04f8ff)",
     boxShadow: "20px 20px 60px #03c5cf, -20px -20px 60px #05ffff",
   },
+  button: {
+    width: "140px",
+    height: "40px",
+    justifyContent: "space-around"
+  }
 }));
 
 export default LoginBox;
