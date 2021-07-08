0ed467496f51a999760dc2f2dfdd20a02e972415
Clifford Morin <cemorin21@gmail.com>
2021-07-08 17:52:04 -0400

    added confirmation page

---
+++ client/src/pages/Confirmation.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import Confirm from "../components/ConfirmationComponents/Confirm"
+
+
+
+
+const Confirmation = () => {
+  return (
+     <Confirm/>
+  )
+};
+
+export default Confirmation;
