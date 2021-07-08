e093cf59f184ea1a1a4c0f54aa8272dfbe7ad53c
Clifford Morin <cemorin21@gmail.com>
2021-07-08 19:01:03 -0400

    fixed nav bar to only render profile when logged in, and only render login when logedout

--- client/src/components/BurgerComponents/Navigation.js
+++ client/src/components/BurgerComponents/Navigation.js
@@ -2,17 +2,132 @@ import * as React from "react";
 import { motion } from "framer-motion";
 import { MenuItem } from "./MenuItem";
 import { NavAnim } from "./BurgerAnimation";
+//Icons
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faHome,
+  faCalendarAlt,
+  faCloudSun,
+  faSignInAlt,
+  faUserPlus,
+  faUserAstronaut
+} from "@fortawesome/free-solid-svg-icons";
+import {
+  faNewspaper
+} from "@fortawesome/free-regular-svg-icons"
 
 
+// export const Navigation = () => (
+//   <motion.ul variants={NavAnim}>
+//     {itemIds.map((i) => (
+//       <MenuItem i={i} key={i} />
+//     ))}
+//   </motion.ul>
+// );
 
+const loggedIn = false; 
+
+const displayProfile = () => {
+
+  if (loggedIn)
+ {
+   return (
+     <MenuItem navItems={portfolioButton}></MenuItem>
+   )
+ }
+}
+
+const displayLoginOrOut = () => {
+
+
+  if (loggedIn) {
+    return (
+      <MenuItem navItems={logOutButton}></MenuItem>
+    )
+
+  }
+    return (
+      <MenuItem navItems={logInButton}></MenuItem>
+    )
+
+ 
+}
 
 export const Navigation = () => (
   <motion.ul variants={NavAnim}>
-    {itemIds.map((i) => (
-      <MenuItem i={i} key={i} />
+    {navItems.map((navitem) => (
+      <MenuItem navItems={navitem} key={navitem.id}/>
     ))}
+
+    {displayProfile()}
+    {displayLoginOrOut()}
   </motion.ul>
 );
 
-const itemIds = [0, 1, 2, 3, 4, 5, 6];
 
+
+const navItems = [
+  {
+    colors: "#FF008C",
+    links: "/",
+    text: "Home",
+    icons: faHome,
+    id: 0
+  },
+  {
+    colors: "#D309E1",
+    links: "/booking",
+    text: "Booking",
+    icons: faCalendarAlt,
+    id: 1
+  },
+  {
+    colors: "#9C1AFF",
+    links: "/weather",
+    text: "Weather",
+    icons: faCloudSun,
+    id: 2
+  },
+  {
+    colors: "#7700FF",
+    links: "/news",
+    text: "News",
+    icons: faNewspaper,
+    id: 3
+  },
+
+  // {
+  //   colors: "#0d00ff",
+  //   links: "/signup",
+  //   text: "SignUp",
+  //   icons: faUserPlus,
+  //   id: 6
+  // },
+ 
+]
+
+
+const portfolioButton =  {
+  colors: "#5a0cd7",
+  links: "/profile",
+  text: "Profile",
+  icons: faUserAstronaut,
+  id: 7
+}
+
+const logInButton =   {
+  colors: "#4400FF",
+  links: "/login",
+  text: "Login",
+  icons: faSignInAlt,
+  id: 4
+}
+
+
+const logOutButton =   {
+  colors: "#4400FF",
+  links: "/login",
+  text: "Logout",
+  icons: faSignInAlt,
+  id: 4
+}
---
+++ client/src/utils/ArticleContext.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const ArticleContext = React.createContext({
+  title: "",
+  url: ""
+});
+
+export default ArticleContext;
