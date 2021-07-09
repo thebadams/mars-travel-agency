b0d335c322fde208c123ca11b70a0ac00d0282b7
Brian Adams <bpeteradams@gmail.com>
2021-07-08 21:32:32 -0400

    wrote rudimentary reducer function

---
+++ client/src/utils/globalUserReducer.js
@@ -0,0 +1,38 @@
+/* Expects action to follow following format:
+action = {
+  type: "",
+  value: ""
+}*/
+
+const reducer = (state, action) => {
+  switch(action.type){
+
+    case 'LOG_IN':
+      return { ...state,
+      loggedIn: true,
+    user: action.value}
+    
+    case 'LOG_OUT':
+      return { ...state,
+      loggedIn: false,
+    user: action.value }
+    
+    case 'SET_SUCCESS_MESSAGE':
+      return { ...state,
+      successMessage: action.value
+      }
+    case 'SET_ERROR_MESSAGE':
+      return { ...state,
+      
+        errorMessage: action.value }
+    
+    case 'RESET_MESSAGES':
+      return { ...state, 
+      successMessage: "",
+      errorMessage: "" }
+    default:
+      return state
+
+  }
+}
+
