9e2f1d403ae73e7f231039945718f4382112420d
Brian Adams <bpeteradams@gmail.com>
2021-07-08 22:15:15 -0400

    developed global context provider and dispatcher

--- client/src/utils/GlobalContext.js
+++ client/src/utils/GlobalContext.js
@@ -1,3 +1,4 @@
+import { response } from 'express';
 import React, { createContext, useContext, useReducer} from 'react';
 import reducer from './globalUserReducer';
 const AppStateContext = createContext({
