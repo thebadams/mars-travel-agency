5827b17ca85574251aad8b03aab2dd963c211741
Jasper Abarquez <78329142+KuyaJasper@users.noreply.github.com>
2021-07-10 12:07:38 -0400

    added text next to image carosel, added styling structure

--- client/src/components/WeatherComponents/weatherCarosel.js
+++ client/src/components/WeatherComponents/weatherCarosel.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import { makeStyles, useTheme } from '@material-ui/core/styles';
 import MobileStepper from '@material-ui/core/MobileStepper';
-import Paper from '@material-ui/core/Paper';
-import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft';
 import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight';
@@ -40,11 +38,13 @@ const tutorialSteps = [
 ];
 
 const useStyles = makeStyles((theme) => ({
+
   root: {
       maxWidth: 400,
     flexGrow: 1,
     marginLeft: '30%',
     marginTop: '5%',
+    fontFamily: 'Roboto-Regular, HelveticaNeue, Arial, sans-serif',
   },
   header: {
     display: 'flex',
@@ -82,9 +82,6 @@ function SwipeableTextMobileStepper() {
 
   return (
     <div className={classes.root}>
-      <Paper square elevation={0} className={classes.header}>
-        <Typography>{tutorialSteps[activeStep].label}</Typography>
-      </Paper>
       <AutoPlaySwipeableViews
         axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
         index={activeStep}
--- client/src/pages/Weather.js
+++ client/src/pages/Weather.js
@@ -3,29 +3,53 @@ import WeatherCard from "../components/WeatherComponents/weatherCard";
 import SwipeableTextMobileStepper from "../components/WeatherComponents/weatherCarosel";
 import styled from "styled-components";
 import MarsLandscape from "../assets/img/MarsLandscape.jpg";
-import { style } from "@material-ui/system";
+import WeatherInfo from "../components/WeatherComponents/weatherInfo";
+
 
 const WeatherStyle = styled.div `
-display: flex;
+/* display: flex;
 flex-direction: column;
+justify-content: center;
+align-items: center; */
+font-family: Roboto-Regular, HelveticaNeue, Arial, sans-serif;
+height: 100vh;
+`
+const WeatherTop = styled.div `
+display: flex;
+flex-direction: row;
+justify-content: center;
+align-items: flex-end;
+padding-left: 20%;
+`
+const WeatherBottom = styled.div `
+display: flex;
+flex-direction: row;
 justify-content: center;
 align-items: center;
 `
+
 const WeatherBgImg = styled.div `
 background-image: url(${MarsLandscape});
 background-repeat: no-repeat;
 background-size: cover;
 width: 100vw;
   height: 100vh
-
 `
 
 const Weather = () => {
   return (
       <WeatherBgImg>
     <WeatherStyle>
+
+    <WeatherTop>
+    <WeatherInfo/>
       <SwipeableTextMobileStepper />
+      </WeatherTop>
+
+      <WeatherBottom>
       <WeatherCard />
+      </WeatherBottom>
+
     </WeatherStyle>
     </WeatherBgImg>
   );
