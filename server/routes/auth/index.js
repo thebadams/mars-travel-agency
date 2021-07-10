afbc28a778bcf1a4d77958a2a9cf127915854749
Brian Adams <bpeteradams@gmail.com>
2021-07-10 12:43:00 -0400

    refactored routes for authrization

---
+++ server/routes/auth/index.js
@@ -0,0 +1,23 @@
+const router = require('express').Router();
+const localRoutes = require('./localRoutes')
+con
+router.use('/local', localRoutes)
+router.use('/facebook', facebookRoutes);
+
+router.get('/logout', function(req, res){
+  req.logout();
+  res.redirect('http://localhost:3000/');
+});
+
+router.get('/session', (req, res) => {
+  if(req.session.user) {
+    res.json({ user:req.session.user, loggedIn: true });
+  } else {
+    res.json({
+      loggedIn: false,
+      user: {}
+    })
+  }
+})
+
+module.exports = router;
