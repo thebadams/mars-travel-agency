63deff9464d1ed681c87efaada183f9fa77e3111
Clifford Morin <cemorin21@gmail.com>
2021-07-12 00:49:36 -0400

    added ticket.js to profile page

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,23 +1,16 @@
 import React from "react";
 import styled from "styled-components";
+import Ticket from "../components/ProfileComponents/Ticket";
 
 
 const ProfileStyle = styled.div`
-    color: blue;
-    font-size: 150px;
     
-.text{
-    display: flex;
-    justify-content: center;
-}
 `;
 
 const Profile = () => {
   return (
       <ProfileStyle>
-          <div className="text">
-          <h1>Hello Tak</h1>
-          </div>
+          <Ticket />
       </ProfileStyle>
   )
 };
