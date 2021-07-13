f4deead8cc7110c4d6013f16384e65544c08d548
Brian Adams <bpeteradams@gmail.com>
2021-07-13 14:58:13 -0400

    minor adjustments to the local authorization strategy layout

--- server/routes/auth/localRoutes.js
+++ server/routes/auth/localRoutes.js
@@ -1,14 +1,14 @@
 const router = require('express').Router();
 const passport = require('../../config/passport')
-
+const db = require('../../models')
 router.post('/register', async (req, res) => {
   console.log(req.body)
   const {email, firstName, lastName, password} = req.body;
   //pass in the user's email, username
-  const user = new User({email, firstName, lastName});
+  const user = new db.User({email, firstName, lastName});
   //create hte user, by passing in the user object and the password to the register method
   try {
-      const registered = await User.register(user, password);
+      const registered = await db.User.register(user, password);
       req.logIn(registered, (error) => {
         error ? res.json(error) : console.log(registered)
       });
