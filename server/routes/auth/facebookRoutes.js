0ac5ad12b9ed488b22bb9adbf73c89baa91eeef6
Brian Adams <bpeteradams@gmail.com>
2021-07-13 19:06:51 -0400

    worked on facebook routes

--- server/routes/auth/facebookRoutes.js
+++ server/routes/auth/facebookRoutes.js
@@ -4,9 +4,9 @@ const passport = require('../../config/passport')
 router.get('/', passport.authenticate('facebook', {scope: ['email']}));
 
 router.get('/callback', passport.authenticate('facebook', {failureRedirect: 'http://localhost:3000/signin'}), (req, res) => {
-
+  req.user ? console.log("Yes") : console.log("No")
   req.session.user = req.user
-  res.redirect('http://localhost:3000/dashboard')
+  res.redirect('http://localhost:3000/#profile')
 })
 
 module.exports = router;
