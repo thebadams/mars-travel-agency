e8bd35260aca5f918c7d7a7f795ca551d1907044
Brian Adams <bpeteradams@gmail.com>
2021-07-13 16:38:32 -0400

    adjusted ffacebook routes

--- server/routes/auth/facebookRoutes.js
+++ server/routes/auth/facebookRoutes.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
 const passport = require('../../config/passport')
+
 router.get('/', passport.authenticate('facebook', {scope: ['email']}));
 
-router.get('/callback', passport.authenticate('facebook', {failureRedirect: 'http://localhost:3000/failure'}), (req, res) => {
-  res.redirect('http://localhost:3000/success')
+router.get('/callback', passport.authenticate('facebook', {failureRedirect: 'http://localhost:3000/signin'}), (req, res) => {
+  req.session.user = req.user
+  res.redirect('http://localhost:3000/dashboard')
 })
 
 module.exports = router;
