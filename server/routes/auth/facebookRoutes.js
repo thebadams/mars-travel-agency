2d9a1c2daa073683619181feff0a745446fdd7bb
Brian Adams <bpeteradams@gmail.com>
2021-07-13 16:05:01 -0400

    fixed facebook button in client

--- server/routes/auth/facebookRoutes.js
+++ server/routes/auth/facebookRoutes.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
 const passport = require('../../config/passport')
-router.get('/auth/facebook', passport.authenticate('facebook', {scope: ['email']}));
+router.get('/', passport.authenticate('facebook', {scope: ['email']}));
 
-router.get('/auth/facebook/callback', passport.authenticate('facebook', {failureRedirect: 'http://localhost:3000/failure'}), (req, res) => {
+router.get('/callback', passport.authenticate('facebook', {failureRedirect: 'http://localhost:3000/failure'}), (req, res) => {
   res.redirect('http://localhost:3000/success')
 })
 
