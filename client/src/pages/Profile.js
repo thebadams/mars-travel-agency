65b875b513131ed5d01ab49b92288e908096f297
Jasper Abarquez <78329142+KuyaJasper@users.noreply.github.com>
2021-07-13 21:01:20 -0400

    started profile page styling

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,18 +1,36 @@
 import React, {useEffect} from "react";
 import styled from "styled-components";
+import { makeStyles } from '@material-ui/core/styles';
 import Grid from "@material-ui/core/Grid"
 import Paper from "@material-ui/core/Paper"
 import ProfileTicket from "../components/ProfileComponents/ProfileTicket";
 import ProfileCard from "../components/ProfileComponents/ProfileCard"
 import { useAppStateContext } from "../utils/GlobalContext";
-import axios from 'axios'
+import axios from 'axios';
+import Earth from '../assets/img/Earth.jpg'
 
-
+//StyleS
 const ProfileStyle = styled.div`
-    
+background-image: url(${Earth});
 `;
 
-const Profile = () => {
+
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    marginTop: '20%',
+    marginBottom: '2%',
+  },
+  test:{
+    background: 'red',
+  },
+
+}));
+
+
+export default function Profile () {
+  const classes = useStyles;
   const [state, dispatch] = useAppStateContext();
   const getSession = async () => {
     const sessionData = await axios.get('/auth/session')
@@ -30,11 +48,12 @@ const Profile = () => {
 
   
   return (  
+    <ProfileStyle>
     <Grid container>
       <Grid item xs={4}>
       </Grid>
       <Grid item xs={6} md={4}>
-        <Paper>
+        <Paper className={classes.test}>
           <ProfileCard name={state.user.firstName} />
           <ProfileTicket />
         </Paper>
@@ -43,8 +62,7 @@ const Profile = () => {
         
         </Grid>
     </Grid>
+    </ProfileStyle>
   
   )
 };
-
-export default Profile;
