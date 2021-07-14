c4955892434d262904f2d58326c3d26c81d2c854
bdurham227 <bdurham227@gmail.com>
2021-07-13 21:03:18 -0400

    weather dashboard rendering photos from api

--- client/src/pages/News.js
+++ client/src/pages/News.js
@@ -1,20 +1,93 @@
 import React, { useEffect, useState } from "react";
-const BASEURL = "https://spaceflightnewsapi.net/api/v2/reports";
+import { Carousel } from "react-bootstrap";
+import styled from 'styled-components';
+import { getSpaceNews } from '.././utils/API';
+import { getMarsPhotos } from ".././utils/API";
+
+
 
 
 //Page Components
 
+
 //Animations
 
-const News = () => {
-
-// const [ newsState, setNewsState ] = useState({ news: res.data});
+// const News = () => {
+// // const newsData = res.data;
+// // console.log(newsData);
+// const [news, setNewsState ] = useState([]);
 
 // useEffect(() => {
-//   axios.get(BASEURL)
-// })
+//   let mounted = true;
+//  getSpaceNews()
+//  .then(items => {
+//    if(mounted) {
+//      setNewsState(items)
+//    }
+//    console.log(items)
+//  })
+//  return () => mounted = false;
+// }, [])
 
-  return <h1>News</h1>;
-};
+
+//   return (
+// <Carousel activeIndex={news} onSelect={useEffect}>
+//   {news.map(item => {
+//     return (
+//       <>
+//     <Carousel.Item>
+//         <img
+//           className="d-block w-10"
+//           src={item.imageUrl}
+//           alt="First slide"
+//         />
+//         <Carousel.Caption>
+//           <h3>{item.title}</h3>
+//           <p>{item.summary}</p>
+//         </Carousel.Caption>
+  
+//       </Carousel.Item>
+//       </>
+//     )
+//   })}
+
+//     </Carousel>
+ 
+//   )
+ 
+// };
+
+
+const News = () => {
+  const [photoState, setPhotoState ] = useState([]);
+
+  useEffect(() => {
+    
+   getMarsPhotos()
+   .then(data => {
+       setPhotoState(data.data.photos)
+     console.log(data.data.photos)
+   })
+  
+  }, [])
+ 
+
+  return (
+    <div>
+      {photoState.map(element => <img src={element.img_src}></img>)}
+    </div>
+  
+  )
+
+} 
+
+
+
+
+
+
+
+
+
 
 export default News;
--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -3,17 +3,27 @@ const key = "2NwgS8Gs1SBh2qJTVSC23AUBBJM0djehB1MrdKZE";
 const insightURL = `https://api.nasa.gov/insight_weather/?api_key=${key}&feedtype=json&ver=1.0`;
 const spaceNewsAPI = "https://spaceflightnewsapi.net/api/v2/reports";
 
-export default {
-  getWeather: function() {
-    return axios.get(insightURL)
-  },
-  getSpaceNews: function() {
-    return axios.get(spaceNewsAPI);
-  },
-  getFlights: function() {
-    return axios.get("/api/booking")
-  },
-  getFlight: function(id) {
-    return axios.get("/api/booking/" + id)
-  }
+// export default {
+//   getWeather: function() {
+//     return axios.get(insightURL)
+//   },
+//   getFlights: function() {
+//     return axios.get("/api/booking")
+//   },
+//   getFlight: function(id) {
+//     return axios.get("/api/booking/" + id)
+//   }
+// };
+export function getSpaceNews() {
+  return fetch("https://spaceflightnewsapi.net/api/v2/reports")
+  .then(data => 
+    data.json())
+  
 };
+
+export async function getMarsPhotos() {
+  const apiKey = "2NwgS8Gs1SBh2qJTVSC23AUBBJM0djehB1MrdKZE";
+  const response = await axios.get(`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?api_key=${apiKey}&sol=1000&page=25`)
+  return response;
+  
+  };
