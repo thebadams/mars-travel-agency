67739f7101b13954aefc7afb4f627dd2160d4d9c
Jasper Abarquez <78329142+KuyaJasper@users.noreply.github.com>
2021-07-13 21:54:10 -0400

    returned user.js

---
+++ server/models/user.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const passportLocalMongoose = require('passport-local-mongoose')
+const Schema = mongoose.Schema
+const userSchema = new Schema({
+  firstName:{
+    type: String,
+    required: [true, "Please Enter a First Name"]
+  },
+  lastName: {
+    type: String,
+    required: [true, "Please Enter a Last Name"]
+  },
+  email: {
+    type: String,
+    required: [true, "please enter an email"]
+  },
+  facebookId: {
+    type: String
+  },
+  reservations: [{
+    type: Schema.Types.ObjectId,
+    ref: "Reservation"
+    }],
+  // password: {
+  //   type: String,
+  //   required: [true, "please enter a password"]
+  // }
+})
+userSchema.plugin(passportLocalMongoose, {
+  usernameField: 'email'
+});
+const User = mongoose.model('User', userSchema)
+module.exports = User;
