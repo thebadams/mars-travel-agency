e6d93f6e87889d9530253dba0ce8b8b3f380f7a2
bdurham227 <bdurham227@gmail.com>
2021-07-20 19:14:16 -0400

    added controllers/routes

--- server/models/index.js
+++ server/models/index.js
@@ -1,8 +1,10 @@
 const Flight = require('./flight');
-const User = require('./user')
+const User = require('./user');
+const Reservation = require('./reservation');
 
 
 module.exports = {
   Flight,
-  User
+  User,
+  Reservation
 };
