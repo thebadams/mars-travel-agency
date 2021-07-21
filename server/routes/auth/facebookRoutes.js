359ebcbf6fc4be918bda1f250a13fb9bdab8b638
Brian Adams <bpeteradams@gmail.com>
2021-07-20 23:20:52 -0400

    got facebook auth working again

--- server/routes/auth/facebookRoutes.js
+++ server/routes/auth/facebookRoutes.js
@@ -6,7 +6,7 @@ router.get('/', passport.authenticate('facebook', {scope: ['email']}));
 router.get('/callback', passport.authenticate('facebook', {failureRedirect: '/signin'}), (req, res) => {
   req.user ? console.log("Yes") : console.log("No")
   req.session.user = req.user
-  // res.redirect('/profile')
+  res.redirect('/profile')
 })
 
 module.exports = router;
