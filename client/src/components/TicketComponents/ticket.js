63774274586b2b8eea5b71006f2fb40da54be1ed
TMats <takomats@hotmail.com>
2021-07-21 19:57:04 -0400

    cleaned up the ticket.js file.

--- client/src/components/TicketComponents/ticket.js
+++ client/src/components/TicketComponents/ticket.js
@@ -1,13 +1,10 @@
-import React, { useEffect, useState, Component } from "react";
+import React, { useState } from "react";
 import Button from '@material-ui/core/Button';
 import { Container, Grid } from '@material-ui/core';
 import styled from "styled-components";
-import axios from 'axios';
 import { Link } from "react-router-dom";
 import { useAppStateContext } from "../../utils/GlobalContext";
 import reservation from '../../utils/reservation';
-import { data } from "jquery";
-import { render } from 'react-dom';
 import TicketHeader from '../TicketComponents/ticketHeader';
 
 const FlightStyle = styled.div`
@@ -629,148 +626,6 @@ const Cell = (props) => {
   );
 }
 
-
-// const Header = (props) => {
-//   return (
-//   <Container>
-//     <div id="headerText">Select Flight</div>
-//     <div id="tripDetail">
-//       Your Trip
-//       <div id="tripDest">
-//         {props.abbreviation} - MARS<div id="oneWay">One Way</div>
-//         <div />
-//       </div>
-//       {`${nowDate[0]} ${nowDate[1]} ${nowDate[2]} ${nowDate[3]}`}
-//     </div>
-//     <svg
-//       id="settings"
-//       xmlns="http://www.w3.org/2000/svg"
-//       width="512"
-//       height="512"
-//       viewBox="0 0 512 512"
-//     >
-//       <line
-//         x1="368"
-//         y1="128"
-//         x2="448"
-//         y2="128"
-//         style={{
-//           fill: "none",
-//           stroke: "#000",
-//           strokeLinecap: "round",
-//           strokeLinejoin: "round",
-//           strokeWidth: "32px"
-//         }}
-//       />
-//       <line
-//         x1="64"
-//         y1="128"
-//         x2="304"
-//         y2="128"
-//         style={{
-//           fill: "none",
-//           stroke: "#000",
-//           strokeLinecap: "round",
-//           strokeLinejoin: "round",
-//           strokeWidth: "32px"
-//         }}
-//       />
-//       <line
-//         x1="368"
-//         y1="384"
-//         x2="448"
-//         y2="384"
-//         style={{
-//           fill: "none",
-//           stroke: "#000",
-//           strokeLinecap: "round",
-//           strokeLinejoin: "round",
-//           strokeWidth: "32px"
-//         }}
-//       />
-//       <line
-//         x1="64"
-//         y1="384"
-//         x2="304"
-//         y2="384"
-//         style={{
-//           fill: "none",
-//           stroke: "#000",
-//           strokeLinecap: "round",
-//           strokeLinejoin: "round",
-//           strokeWidth: "32px"
-//         }}
-//       />
-//       <line
-//         x1="208"
-//         y1="256"
-//         x2="448"
-//         y2="256"
-//         style={{
-//           fill: "none",
-//           stroke: "#000",
-//           strokeLinecap: "round",
-//           strokeLinejoin: "round",
-//           strokeWidth: "32px"
-//         }}
-//       />
-//       <line
-//         x1="64"
-//         y1="256"
-//         x2="144"
-//         y2="256"
-//         style={{
-//           fill: "none",
-//           stroke: "#000",
-//           strokeLinecap: "round",
-//           strokeLinejoin: "round",
-//           strokeWidth: "32px"
-//         }}
-//       />
-//       <circle
-//         cx="336"
-//         cy="128"
-//         r="32"
-//         style={{
-//           fill: "none",
-//           stroke: "#000",
-//           strokeLinecap: "round",
-//           strokeLinejoin: "round",
-//           strokeWidth: "32px"
-//         }}
-//       />
-//       <circle
-//         cx="176"
-//         cy="256"
-//         r="32"
-//         style={{
-//           fill: "none",
-//           stroke: "#000",
-//           strokeLinecap: "round",
-//           strokeLinejoin: "round",
-//           strokeWidth: "32px"
-//         }}
-//       />
-//       <circle
-//         cx="336"
-//         cy="384"
-//         r="32"
-//         style={{
-//           fill: "none",
-//           stroke: "#000",
-//           strokeLinecap: "round",
-//           strokeLinejoin: "round",
-//           strokeWidth: "32px"
-//         }}
-//       />
-//     </svg>
-//   </Container>
-//   );
-// };
-
-// const DataArr = Array(5)
-//   .fill(0)
-//   .map(Number.call, Number);
 const Ticket = ({ flights }) => {
   const [state, dispatch ] = useAppStateContext();
 
