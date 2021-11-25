969a3f14749cbee9dda1eb7f93d4a298bbe2a24e
TheBadams <bpeteradams@gmail.com>
2021-11-24 21:05:46 -0500

    TicketModel(tests): Wrote 6 test.todos in Ticket.test.ts

--- v2/server/src/models/Ticket.test.ts
+++ v2/server/src/models/Ticket.test.ts
@@ -13,5 +13,17 @@ describe('Ticket Model', () => {
 		test('seat Property Should Be the Number 1', () =>{
 			expect(newTicket).toHaveProperty('seat', 1);
 		})
+		test.todo('flight Property Should Exist, Referencing A Flight ID');
+		test.todo('ticketHolder should be a Property, referencing a User');
+	})
+	describe('Ticket Validation', () => {
+		describe('Seat Property Requirement', () => {
+			test.todo('Ticket Seat Property Is Required, Should Return a Validation Error')
+			test.todo('When A Validation Error Is Thrown, Return the Message: "Seat Is Required"')
+		})
+		describe('Flight Property Requirement', () => {
+			test.todo('Ticket Flight Property Is Required, Should Return a Validation Error')
+			test.todo('When A Validation Error Is Thrown, Return the Message: "Flight Is Required"')
+		})
 	})
 })
