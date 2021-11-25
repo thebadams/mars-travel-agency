8448ecd4a24ed36db6cb63ad455236079b714083
TheBadams <bpeteradams@gmail.com>
2021-11-25 10:37:10 -0500

    Wrote test.todos for the properties on the User Model

---
+++ v2/server/src/models/User.test.ts
@@ -0,0 +1,12 @@
+import User from './User';
+
+describe('User Model', () => {
+	describe('User Model Properties', () => {
+		test.todo('User Should Have an _id Property')
+		test.todo('User Should have an email Property that is the String "test@test.com"');
+		test.todo('User Should Have a firstName Property, that is the string, "John"')
+		test.todo('User Should Have A lastName Property, that is the String, "Doe"')
+		test.todo('User Should Have a facebookId property, that is the String "FACEBOOK_ID"');
+		test.todo('User Should Have a Ticket Property, that references a Ticket');
+	})
+})
